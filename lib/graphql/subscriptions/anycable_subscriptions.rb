69da1ed7c7e4c0426cbce0b5922130bf4269bb8b
Thomas Lacroix <thomas.lacroix@sorare.com>
2025-05-21 10:30:20 +0200

    Replace tap by then

--- lib/graphql/subscriptions/anycable_subscriptions.rb
+++ lib/graphql/subscriptions/anycable_subscriptions.rb
@@ -165,12 +165,13 @@ module GraphQL
           redis.mapped_hmget(
             "#{redis_key(SUBSCRIPTION_PREFIX)}#{subscription_id}",
             :query_string, :variables, :context, :operation_name
-          ).tap do |subscription|
-            return if subscription.values.all?(&:nil?) # Redis returns hash with all nils for missing key
+          ).then do |subscription|
+            next if subscription.values.all?(&:nil?) # Redis returns hash with all nils for missing key
 
             subscription[:context] = @serializer.load(subscription[:context])
             subscription[:variables] = JSON.parse(subscription[:variables])
             subscription[:operation_name] = nil if subscription[:operation_name].strip == ""
+            subscription
           end
         end
       end
